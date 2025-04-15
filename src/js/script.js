b908c50ddec8fd1b5f978a3fe5115cae3f5969ee
Rogério <rogerio.cruz.arroyo@gmail.com>
2025-04-15 19:48:02 -0300

    realizando exercicio 1

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+/*EXERCÍCIO 1*/
+
+//Declarando um variável com valor undefined 
+let nome;
+console.log(nome);               //saída: undefined
+
+//Declarando uma variável com valor vazio
+let grupo = "";
+console.log(grupo);             //saída: (string vazia, nada aparece)
+
+//Declarando uma variável com valor em JavaScript
+let quantidade = 5;
+console.log(quantidade);       //saída: 5
